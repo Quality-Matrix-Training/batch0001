87da3d5c8de45eae8f0f712ba6097cdcfd4f82cc
NikhilManvesh <89297190+Manvesh-P@users.noreply.github.com>
2021-10-10 22:09:59 +0530

    Update assignment_2.py

--- nikhil_assignment_2/assignment_2.py
+++ nikhil_assignment_2/assignment_2.py
@@ -1,127 +1,169 @@
-def top_ten_and_least_watched_movie_according_to_imdb():
+def leastwatched_and_mostwatched_films():
+    
     import json
 
+    f = open('repeat_imdb.json', mode='r')
+    q = f.read()
+    q1 = json.loads(q)
+    l = []
+    l1 = []
     d = {}
 
-    f = open("assignment.json", "r")
 
-    q = json.load(f)
+    for i in range(0, len(q1)):
+        if q1[i]['name'] == 'The Wizard of Oz' or q1[i]['name'] == 'Batman':
+            l.append(q1[i]['name'])
+            l.append(q1[i]['imdb_score'])
+            l1.append(l)
+            l = []
 
-    for i in range(0, len(q)):
-        d[q[i]['name']] = q[i]['imdb_score']
 
-    # print(d)
+    for j in range(0, len(q1)):
+        d[q1[j]['name']] = q1[j]['imdb_score']
 
-    q1 = list(d.items())
-    q2 = sorted(q1, key=lambda x: x[1], reverse=True)
-    # print(q2)
 
-    print("The top 10 movies are: \n")
-    for i in range(0, len(q2)):
-        if i > 9:
+    for k1, k2 in l1:
+        if d[k1] != k2:
+            d[k1.lower()] = k2
+
+
+    w = d.items()
+    w1 = sorted(w, key=lambda x:x[1])
+    w2 = sorted(w, key=lambda x:x[1], reverse=True)
+
+    print("The least watched movie is:",w1[0][0])
+
+    print("\nThe 10 most watched movies are:")
+    for k in range(0, len(w2)):
+        if k > 9:
             break
         else:
-            print(q2[i][0])
-
-    print("\nThe least watched movie is:", end=' ')
-    s1, s2 = min(q1, key=lambda x: x[1])
-    print(s1)
-
-    f.close()
-
-
-def most_popular_director_in_top_100_movies():
+            print(w2[k][0])
+            
+            
+def most_popular_director_in_top100_movies():
     import json
 
+    f = open('repeat_imdb.json', mode='r')
+    q = f.read()
+    q1 = json.loads(q)
+
+    l = []
+    l1 = []
+    d = {}
     d1 = {}
 
-    f = open("assignment.json", "r")
+    for i in range(0, len(q1)):
+        if q1[i]['name'] == 'The Wizard of Oz' or q1[i]['name'] == 'Batman':
+            l.append(q1[i]['name'])
+            l.append(q1[i]['imdb_score'])
+            l1.append(l)
+            l = []
 
-    q = json.load(f)
 
-    for i in range(0, len(q)):
+    for j in range(0, len(q1)):
+        d[q1[j]['name']] = q1[j]['imdb_score']
+
+    for k1, k2 in l1:
+        if d[k1] != k2:
+            d[k1.lower()] = k2
+
+
+    w = d.items()
+    w1 = sorted(w, key=lambda x:x[1], reverse=True)
+
+    for i in range(0, len(w1)):
         if i > 99:
             break
         else:
-            d1[q[i]['director']] = q[i]['99popularity']
+            e1, e2 = w1[i]
+            for j in range(0, len(q1)):
+                if e1 == 'the wizard of oz':
+                    e1 = 'The Wizard of Oz'
+                    if q1[j]['name'] == e1 and q1[j]['imdb_score'] == e2:
+                        if q1[j]['director'] not in d1:
+                            d1[q1[j]['director']] = [q1[j]['99popularity']]
+                        else:
+                            d1[q1[j]['director']].append(q1[j]['99popularity'])
 
-    # print(d1)
 
-    s = d1.items()
-    s1 = sorted(s, key=lambda x: x[1], reverse=True)
+                elif e1 == 'batman':
+                    e1 = 'Batman'
+                    if q1[j]['name'] == e1 and q1[j]['imdb_score'] == e2:
+                        if q1[j]['director'] not in d1:
+                            d1[q1[j]['director']] = [q1[j]['99popularity']]
+                        else:
+                            d1[q1[j]['director']].append(q1[j]['99popularity'])
 
-    print("The most popular director in 1st 100 movies is:", end=' ')
-    print(s1[0][0])
+                else:
 
+                    if q1[j]['name'] == e1:
+                        if q1[j]['director'] not in d1:
+                            d1[q1[j]['director']] = [q1[j]['99popularity']]
+                        else:
+                            d1[q1[j]['director']].append(q1[j]['99popularity'])
+
+
+
+    w = d1.items()
+    w1 = sorted(w, key=lambda x:sum(x[1])/len(x[1]), reverse=True)
+    print("The most poplular director in top 100 films is:",w1[0][0])
     f.close()
 
-
+    
 def director_with_most_no_of_films():
+    
     import json
 
+    f = open('repeat_imdb.json', mode = 'r')
+    q = f.read()
+    q1 = json.loads(q)
     d = {}
 
-    f = open("assignment.json", "r")
-
-    q = json.load(f)
-
-    # print(q[0])
-
-    for i in range(0, len(q)):
-        if q[i]['director'] not in d:
-            d[q[i]['director']] = 1
+    for i in range(0, len(q1)):
+        if q1[i]['director'] not in d:
+            d[q1[i]['director']] = 1
         else:
-            d[q[i]['director']] += 1
+            d[q1[i]['director']] += 1
 
-    # print(d)
-    x = d.items()
-    x1 = sorted(x, key=lambda x: x[1], reverse=True)
-
-    print("The director with most no of films is:", end=' ')
-    print(x1[0][0])
+    w = d.items()
+    w1 = sorted(w, key=lambda x:x[1], reverse=True)
+    print("The director with most no of films is:",w1[0][0])
 
     f.close()
-
+    
 
 def most_watched_genre():
+    
     import json
 
+    f = open('repeat_imdb.json', mode='r')
+    q = f.read()
+    q1 = json.loads(q)
+    l = []
     d = {}
-    list1 = []
 
-    f = open("assignment.json", "r")
-
-    q = json.load(f)
-
-    for i in range(0, len(q)):
-        h = q[i]['genre']
-        for j in range(0, len(h)):
-            if h[j].strip(' ') not in d:
-                d[h[j].strip(' ')] = 1
+    for i in range(0, len(q1)):
+        e = q1[i]['genre']
+        for j in range(0, len(e)):
+            if e[j].strip(' ') not in d:
+                d[e[j].strip(' ')] = 1
             else:
-                d[h[j].strip(' ')] += 1
+                d[e[j].strip(' ')] += 1
 
-    # print(d)
-
-    q1 = d.items()
-    q2 = sorted(q1, key=lambda x: x[1], reverse=True)
-
-    print("The most watched genre is:", end='')
-    print(q2[0][0])
-
+    w = d.items()
+    w1 = sorted(w, key=lambda x:x[1], reverse=True)
+    print("The most watched genre is:",w1[0][0])
     f.close()
-
+    
+    
+'''   
+Uncomment the below function calls one after the other to invoke their definitions and get the 
+answers for the queries.
 
 '''
-Uncomment each one of the following to know the answers to the queries.
-
-'''
-
-# top_ten_and_least_watched_movie_according_to_imdb()
-
-# most_popular_director_in_top_100_movies()
-
+# leastwatched_and_mostwatched_films()
+# most_popular_director_in_top100_movies()
 # director_with_most_no_of_films()
-
 # most_watched_genre()
 
